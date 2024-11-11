647820010231a124749620067ba5b7a916587369
Teguh Widodo <43848500+teguhwin8@users.noreply.github.com>
2024-11-11 23:58:29 +0700

    update trustHost

--- app/(auth)/auth.config.ts
+++ app/(auth)/auth.config.ts
@@ -9,6 +9,7 @@ export const authConfig = {
     // added later in auth.ts since it requires bcrypt which is only compatible with Node.js
     // while this file is also used in non-Node.js environments
   ],
+  trustHost: true,
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
       let isLoggedIn = !!auth?.user;
